e06a768b80b67da1246698b81ad139bb06122690
Cesar Rocha <cesarvasconcelos@copin.ufcg.edu.br>
2024-09-25 10:01:12 -0300

    Refactor de div para paragrafo

--- javascript/praticas/pratica-livros-v3/index.js
+++ javascript/praticas/pratica-livros-v3/index.js
@@ -48,20 +48,20 @@ function resetarCampos() {
 
 
 function buscarLivroPorTitulo() {
-    let divResultadoBusca = document.getElementById('divResultadoBusca');
+    let parResultadoBusca = document.getElementById('parResultadoBusca');
     const tituloABuscar = document.getElementById('inputTitulo').value.toLowerCase(); // busca case-insensitive aqui
 
     const livroEncontrado = listaLivros.find(livro => livro.titulo.toLowerCase().includes(tituloABuscar));
 
     if (livroEncontrado != null) {
-        divResultadoBusca.innerHTML =
+        parResultadoBusca.innerHTML =
             `<strong>Livro encontrado:</strong><br>\
             Livro ID: ${livroEncontrado.id}<br>\
             Título: ${livroEncontrado.titulo}<br>\
             Ano de Publicação: ${livroEncontrado.ano}`
             ;
     } else {
-        divResultadoBusca.textContent = `Nenhum livro encontrado com o título "${tituloABuscar}".`;
+        parResultadoBusca.textContent = `Nenhum livro encontrado com o título "${tituloABuscar}".`;
     }
 
     // resetar elemento HTML input de busca de título
