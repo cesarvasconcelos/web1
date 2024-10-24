418372a5ec665a38832e42c7aeb988aaa4dfcaba
Cesar Rocha <cesarvasconcelos@copin.ufcg.edu.br>
2024-10-24 08:38:21 -0300

    Refactor pratica-livros-v3

--- javascript/praticas/pratica-livros-v3/index.js
+++ javascript/praticas/pratica-livros-v3/index.js
@@ -1,83 +1,82 @@
-const corpoTabelaBusca = document.getElementById('tblListagemBody'); // HTML elem
+const corpoTabelaListagem = document.getElementById("corpoTabelaListagem"); // HTML elem
 
-const id = document.getElementById('livroID'); // HTML elem
-const título = document.getElementById('livroTitulo'); // HTML elem
-const ano = document.getElementById('livroAno'); // HTML elem
-const inputBuscarTitulo = document.getElementById('inputBuscarTitulo'); // HTML elem
+const id = document.getElementById("livroID"); // HTML elem
+const título = document.getElementById("livroTitulo"); // HTML elem
+const ano = document.getElementById("livroAno"); // HTML elem
+const inputBuscarTitulo = document.getElementById("inputBuscarTitulo"); // HTML elem
 
-let mensagemBusca = document.getElementById('parResultadoBusca');
+let mensagemBusca = document.getElementById("parResultadoBusca");
 let livro;
 let listaLivros = []; // note a lista para armazenar os livros
 
 function efetuarCadastroLivro() {
-    livro = criarLivro(id.value, título.value, ano.value);
+	livro = criarLivro(id.value, título.value, ano.value);
 
-    listaLivros.push(livro);
-    incluirLivroTabelaResultadoBusca();
-    apagarCamposHTMLDadosLivro();
+	listaLivros.push(livro);
+	incluirLivroTabelaResultadoBusca();
+	apagarCamposHTMLDadosLivro();
 }
 
 function criarLivro(umId, umTítulo, umAno) {
-    // criar objeto livro
-    const objetoLivro = {
-        id: umId,
-        titulo: umTítulo,
-        ano: umAno
-    };
+	// criar objeto livro
+	const objetoLivro = {
+		id: umId,
+		titulo: umTítulo,
+		ano: umAno,
+	};
 
-    return objetoLivro;
+	return objetoLivro;
 }
 
 function incluirLivroTabelaResultadoBusca() {
-    const novaLinha = criarNovaLinhaComDadosLivro();
-    corpoTabelaBusca.appendChild(novaLinha);
+	const novaLinha = criarNovaLinhaComDadosLivro();
+	corpoTabelaListagem.appendChild(novaLinha);
 }
 
 function criarNovaLinhaComDadosLivro() {
-    const novaLinha = document.createElement('tr');
-    novaLinha.innerHTML = `<td>${livro.id}</td><td>${livro.titulo}</td><td>${livro.ano}</td>`;
-    return novaLinha;
+	const novaLinha = document.createElement("tr");
+	novaLinha.innerHTML = `<td>${livro.id}</td><td>${livro.titulo}</td><td>${livro.ano}</td>`;
+	return novaLinha;
 }
 
 function apagarCamposHTMLDadosLivro() {
-    // apagar os valores dos campos
-    id.value = '';
-    título.value = '';
-    ano.value = '';
+	// apagar os valores dos campos
+	id.value = "";
+	título.value = "";
+	ano.value = "";
 }
 
 function processarBuscaLivroPorTítulo() {
-    const títuloDesejado = inputBuscarTitulo.value.toLowerCase(); // busca case-insensitive aqui
-    const livroRetornado = buscarLivroNaListaPor(títuloDesejado);
+	const títuloDesejado = inputBuscarTitulo.value.toLowerCase(); // busca case-insensitive aqui
+	const livroRetornado = buscarLivroNaListaPor(títuloDesejado);
 
-    mostrarMensagemResultadoBusca(livroRetornado);
-    apagarCampoHTMLBuscaTítulo();
+	mostrarMensagemResultadoBusca(livroRetornado);
+	apagarCampoHTMLBuscaTítulo();
 }
 
 function buscarLivroNaListaPor(título) {
-    let umLivro = null;
-    if (título)
-        umLivro = listaLivros.find(
-            cadaLivro => cadaLivro.titulo.toLowerCase().includes(título)
-        );
+	let umLivro = null;
+	if (título) {
+		umLivro = listaLivros.find((cadaLivro) =>
+			cadaLivro.titulo.toLowerCase().includes(título)
+		);
+	}
 
-    return umLivro;
+	return umLivro;
 }
 
 function mostrarMensagemResultadoBusca(livro) {
-    if (livro != null) {
-        mensagemBusca.innerHTML =
-            `<strong>Livro encontrado:</strong><br>\
+	if (livro != null) {
+		mensagemBusca.innerHTML = `<strong>Livro encontrado:</strong><br>\
             Livro ID: ${livro.id}<br>\
             Título: ${livro.titulo}<br>\
-            Ano de Publicação: ${livro.ano}`
-            ;
-    } else {
-        mensagemBusca.textContent = `Nenhum livro encontrado.`;
-    }
+            Ano de Publicação: ${livro.ano}`;
+	} else {
+		mensagemBusca.textContent = `Nenhum livro encontrado.`;
+	}
 }
 
 function apagarCampoHTMLBuscaTítulo() {
-    // resetar elemento HTML input de busca de título
-    inputBuscarTitulo.value = '';
+	// resetar elemento HTML input de busca de título
+	inputBuscarTitulo.value = "";
 }
