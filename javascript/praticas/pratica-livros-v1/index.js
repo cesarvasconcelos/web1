0c5232c04d7e033d6b431ae30eff58aa556a87b3
Cesar Rocha <cesarvasconcelos@copin.ufcg.edu.br>
2026-05-15 14:32:58 -0300

    refactor(index.html, index.js): standardize input IDs and enhance variable naming for consistency

--- javascript/praticas/pratica-livros-v1/index.js
+++ javascript/praticas/pratica-livros-v1/index.js
@@ -1,33 +1,33 @@
 function cadastrarLivro() {
     // obter uma referência ao elemento tbody da table de listagem
-    const tblListagemBody = document.getElementById('tblListagemBody');
+    const corpoTabela = document.getElementById('corpoTabelaListagem');
 
     // Ler os valores dos dados do livro
-    const livroID = document.getElementById('livroID').value; // String
-    const título = document.getElementById('livroTitulo').value; // String
-    const ano = document.getElementById('livroAno').value; // String
+    const livroId = document.getElementById('inputLivroId').value; // String
+    const titulo = document.getElementById('inputLivroTitulo').value; // String
+    const ano = document.getElementById('inputLivroAno').value; // String
 
-    // Criar novos elementos HTML: nova linha e suas colunas de dados
+    // Criar novos elementos HTML: nova linha e suas células de dados
     const novaLinha = document.createElement('tr');
-    const colunaID = document.createElement('td');
-    const colunaTitulo = document.createElement('td');
-    const colunaAno = document.createElement('td');
+    const celulaId = document.createElement('td');
+    const celulaTitulo = document.createElement('td');
+    const celulaAno = document.createElement('td');
 
     // Injetar os valores lidos nas células recém-criadas
-    colunaID.textContent = livroID;
-    colunaTitulo.textContent = título;
-    colunaAno.textContent = ano;
+    celulaId.textContent = livroId;
+    celulaTitulo.textContent = titulo;
+    celulaAno.textContent = ano;
 
-    // fazer o 'append' da coluna dentro da linha
-    novaLinha.appendChild(colunaID);
-    novaLinha.appendChild(colunaTitulo);
-    novaLinha.appendChild(colunaAno);
+    // fazer o 'append' da célula dentro da linha
+    novaLinha.appendChild(celulaId);
+    novaLinha.appendChild(celulaTitulo);
+    novaLinha.appendChild(celulaAno);
 
     // fazer um 'append' da linha na tabela
-    tblListagemBody.appendChild(novaLinha);
+    corpoTabela.appendChild(novaLinha);
 
-    // apagar os valores dos campos
-    document.getElementById('livroID').value = '';
-    document.getElementById('livroTitulo').value = '';
-    document.getElementById('livroAno').value = '';
+    // limpar os valores dos campos
+    document.getElementById('inputLivroId').value = '';
+    document.getElementById('inputLivroTitulo').value = '';
+    document.getElementById('inputLivroAno').value = '';
 }
